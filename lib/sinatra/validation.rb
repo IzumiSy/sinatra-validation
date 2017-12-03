b941a4ddcd4ae62c89a39ea949341f1344787c1c
IzumiSy <beetle-noise@gmx.com>
2017-12-03 20:05:22 +0900

    Fix namespacing

--- lib/sinatra/validation.rb
+++ lib/sinatra/validation.rb
@@ -7,18 +7,20 @@ module Sinatra
       attr_accessor :params, :messages
     end
 
+    class Result < Struct.new("Result", :params, :messages)
+    end
+
     module Helpers
       def validates(silent = false, &block)
         schema = Dry::Validation.Schema(&block)
-        result = Struct.new("Result", :params, :messages)
         validation = schema.call(params)
 
         if validation.success?
-          return result.new(validation.output)
+          return Result.new(validation.output)
         end
 
         errors = validation.messages(full: true).values.flatten;
-        return result.new(validation.output, errors) if silent
+        return Result.new(validation.output, errors) if silent
         raise InvalidParameterError.new(params: validation.output, messages: errors)
       end
     end
