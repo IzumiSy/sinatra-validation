c80be82053022f014e8a09135b11998ffc5443d1
IzumiSy <beetle-noise@gmx.com>
2017-12-03 20:26:29 +0900

    Implement silent_validation option and default exception handling

--- lib/sinatra/validation.rb
+++ lib/sinatra/validation.rb
@@ -20,13 +20,24 @@ module Sinatra
         end
 
         errors = validation.messages(full: true).values.flatten;
-        return Result.new(validation.output, errors) if silent
+        if silent || settings.silent_validation
+          return Result.new(validation.output, errors)
+        end
         raise InvalidParameterError.new(params: validation.output, messages: errors)
+      rescue InvalidParameterError => exception
+        error = exception.to_s
+
+        if content_type && content_type.match(mime_type(:json))
+          error = { errors: errors }.to_json
+        end
+
+        halt 400, error
       end
     end
 
     def self.registered(app)
       app.helpers Validation::Helpers
+      app.set :silent_validation, false
     end
   end
 end
