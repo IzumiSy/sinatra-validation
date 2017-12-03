e1f0351fab65a8276f210c19b885d62fc1344b4c
IzumiSy <beetle-noise@gmx.com>
2017-12-03 20:12:54 +0900

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,16 @@
-require "bundler/setup"
-require "sinatra/validation"
+require 'bundler/setup'
+require 'sinatra/validation'
+require_relative '../example/app.rb'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  def app
+    Application.new
   end
 end
