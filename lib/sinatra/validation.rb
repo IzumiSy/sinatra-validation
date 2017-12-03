6697d3db31829302f86ec2bbfe754d01feead9bb
IzumiSy <beetle-noise@gmx.com>
2017-12-03 17:59:09 +0900

    Implement validates helper

--- lib/sinatra/validation.rb
+++ lib/sinatra/validation.rb
@@ -1,7 +1,28 @@
 require "sinatra/validation/version"
+require "sinatra/errorcodes"
+require "dry-validation"
 
 module Sinatra
   module Validation
-    # Your code goes here...
+    module Helpers
+      def validates(&block)
+        schema = Dry::Validation.Schema(&block)
+        validation = schema.call(params)
+
+        if validation.success?
+          return validation.output
+        end
+
+        if defined?(HTTPError)
+          raise HTTPError::BadRequest
+        else
+          halt 400, 'Bad Request'
+        end
+      end
+    end
+
+    def self.registered(app)
+      app.helpers Validation::Helpers
+    end
   end
 end
