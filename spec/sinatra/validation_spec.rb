4fb039af431d3db01e405b0185482e8152b15b95
IzumiSy <beetle-noise@gmx.com>
2017-12-03 21:23:57 +0900

    Implement specs

--- spec/sinatra/validation_spec.rb
+++ spec/sinatra/validation_spec.rb
@@ -5,7 +5,24 @@ RSpec.describe Sinatra::Validation do
     expect(Sinatra::Validation::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "returns 200" do
+    get '/basic?name=justine'
+    expect(last_response.status).to eql(200)
+  end
+
+  it "returns 400 if the parameters needed" do
+    get '/basic'
+    expect(last_response.status).to eql(400)
+  end
+
+  it "returns 200 with `silent` option enabled" do
+    get '/silent'
+    expect(last_response.status).to eql(200)
+  end
+
+  # TODO here needs to check if Sinatra::Validation::InvalidParameterError is thrown or not
+  it "return 500 with `raise` option enabled" do
+    get '/raise'
+    expect(last_response.status).to eql(500)
   end
 end
