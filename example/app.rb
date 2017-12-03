95aa03289a06c7ba761630eb1bb1d4bbb743dae6
IzumiSy <beetle-noise@gmx.com>
2017-12-03 18:11:54 +0900

    Implement example

--- example/app.rb
+++ example/app.rb
@@ -1,1 +1,21 @@
 require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/validation'
+
+class Application < Sinatra::Base
+  configure do
+    register Sinatra::Validation
+  end
+
+  before do
+    content_type :json
+  end
+
+  get '/user' do
+    permitted_params = validates do
+      required("name").filled(:str?)
+    end
+
+    body "name: #{permitted_params[:name]}"
+  end
+end
